885eae8eda32a0f7c935b44c9dd856977f5ce5a9
nghia46203 <lenghia69hm@gmail.com>
2024-05-12 23:20:29 +0700

    update 2

---
+++ ReactJs/component/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import React from 'react';
+import  Header from '../component/Header/Header.js';
+
+function App(){
+  return(
+    <div>
+      <h4>anh nghia hoc react Lor</h4>
+        <Header/>
+    </div>
+  )
+}
+
+
+
+export default App;
